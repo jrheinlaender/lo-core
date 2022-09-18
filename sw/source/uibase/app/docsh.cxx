415f61367712ec03f007315de247d0af82af8b96
Jan Rheinländer <jrheinlaender@users.sourceforge.net>
2022-09-18 20:19:30 +0200

    iMath: Get previous iFormula from the PreviousIFormula property
    Change-Id: I3a4e4b23b2b43a9974c54c81f6fb31b1bad04520

--- sw/source/uibase/app/docsh.cxx
+++ sw/source/uibase/app/docsh.cxx
@@ -1419,7 +1419,8 @@ void SwDocShell::RemoveIFormula(const OUString& formulaName) {
     if (formulaIterator == m_IFormulaNames.end()) return; // See SwUndoFlyBase::DelFly() why this can happen
 
     SAL_INFO("sw.imath", "Removing iFormula " << formulaName);
-    OUString previousName = FindPreviousIFormulaName(formulaName);
+    Reference< XComponent > xFormulaComp = getObjectByName(GetModel(), *formulaIterator);
+    OUString previousName = getFormulaProperty(xFormulaComp, "PreviousIFormula");
     std::list< OUString >::iterator next_it = m_IFormulaNames.end();
 
     while (formulaIterator != m_IFormulaNames.end()) {
@@ -1429,9 +1430,9 @@ void SwDocShell::RemoveIFormula(const OUString& formulaName) {
     }
 
     if (next_it != m_IFormulaNames.end()) {
-        Reference< XComponent > xFormulaComp = getObjectByName(GetModel(), *next_it);
-        setFormulaProperty(xFormulaComp, "PreviousIFormula", makeAny(previousName));
-        RecalculateDependentIFormulas(*next_it, "");
+        xFormulaComp = getObjectByName(GetModel(), *next_it);
+        setFormulaProperty(xFormulaComp, "PreviousIFormula", uno::makeAny(previousName));
+        RecalculateDependentIFormulas(*next_it, getFormulaProperty(xFormulaComp, "iFormula")); // next_it has already been compiled because the previous iFormula was changed
     }
 }
 

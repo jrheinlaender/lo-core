6c81d60ee7d7f105733c2fcead811d715e2d8c2a
Jan Rheinländer <jrheinlaender@users.sourceforge.net>
2022-11-13 08:27:52 +0100

    iMath: Fix cln, ginac and imath build dependencies Change-Id: I0d21080e269b583ef3ccad841fb03bd7691111d7

--- sw/Library_msword.mk
+++ sw/Library_msword.mk
@@ -50,7 +50,6 @@ $(eval $(call gb_Library_use_libraries,msword,\
     editeng \
     i18nlangtag \
     i18nutil \
-    imath \
     msfilter \
     sal \
     sax \
@@ -77,8 +76,6 @@ $(eval $(call gb_Library_use_externals,msword,\
 	icuuc \
 	icu_headers \
 	libxml2 \
-        cln \
-        ginac \
 ))
 
 $(eval $(call gb_Library_add_exception_objects,msword,\

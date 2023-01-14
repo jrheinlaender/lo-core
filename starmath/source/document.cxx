ac2dcc8e657668c849e71daa335b4e9f1876693a
Jan Rheinländer <jrheinlaender@users.sourceforge.net>
2023-01-14 21:30:24 +0100

    iMath: Fix a number of minor issues, and improve logging
    Change-Id: I0e9b0ccbc589aa3a38e05462bfa87415ede2e2a7

--- starmath/source/document.cxx
+++ starmath/source/document.cxx
@@ -135,7 +135,6 @@ namespace GiNaC {
 class iMathDoc;
 class documentObject;
 #define FORMULAOBJECT SmDocShell
-#define DOCUMENTOBJECT documentObject
 #include <imath/smathparser.hxx>
 
 #include <iostream>

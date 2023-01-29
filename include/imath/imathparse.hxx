e34d2198750fd7e793848c0d741f1b92223e4a2b
Jan Rheinländer <jrheinlaender@users.sourceforge.net>
2023-01-29 18:16:05 +0100

    iMath: Code updates from extension
    Change-Id: I801071a25e55377db2439beec7511f8d5c2f6d53

---
+++ include/imath/imathparse.hxx
@@ -0,0 +1,72 @@
+/***************************************************************************
+    imathparse.hxx  -  Parser definition for smathparser and smathlexer
+                             -------------------
+    begin                : Mon Jan 23 2023
+    copyright            : (C) 2023 by Jan Rheinlaender
+    email                : jrheinlaender@users.sourceforge.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef IMATHPARSE_H
+#define IMATHPARSE_H
+
+#ifdef INSIDE_SM
+  #include <imath/eqc.hxx>
+  #include <imath/iFormulaLine.hxx>
+#else
+  #include <eqc.hxx>
+  #include <iFormulaLine.hxx>
+#endif
+
+namespace imath {
+    // Prototype for the lexing function. This must match the declaration of lex_param at the top of smathparser.y
+    #define YY_DECL imath::smathparser::token::yytokentype imathlex (imath::smathparser::semantic_type* yylval, \
+                imath::smathparser::location_type* yylloc, std::shared_ptr<eqc> compiler, \
+                unsigned include_level)
+
+    struct IMATH_DLLPUBLIC parserParameters {
+    // Input
+        /// Document access
+        css::uno::Reference<css::uno::XComponentContext> xContext;
+        css::uno::Reference<css::frame::XModel> xDocumentModel;
+
+        /// the raw formula text from the UI
+        OUString rawtext;
+
+        /// Is copy+paste mode active in the parent document (important to avoid crashes during recalculation)
+        bool copyPasteActive;
+
+    // Input and output
+        /// The parsed formula text split into lines
+        std::list<iFormulaLine_ptr>* lines;
+        /// The compiler
+        std::shared_ptr<eqc> compiler;
+        /// The options
+        std::shared_ptr<GiNaC::optionmap> global_options;
+
+    // Output
+        /// The compiled equations of the iFormula are cacheable (saving time on re-compilation)
+        bool cacheable; // TODO: Caching is not implemented yet
+        /// The results of the last compilation (for cacheable iFormulas only)
+        std::vector<std::pair<std::string, GiNaC::expression> >* cached_results;
+
+        /// List of formulas for which an update should be inserted
+        std::list<OUString> updateFormulas;
+
+        /// Error string
+        OUString errormessage;
+    };
+
+    // Front-end to the parser
+    IMATH_DLLPUBLIC int parse(parserParameters& params);
+}
+
+#endif

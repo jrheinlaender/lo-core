fd856ffc4b67aead0d2078e68859f24035bc608f
Jan Rheinländer <jrheinlaender@users.sourceforge.net>
2023-03-28 17:54:33 +0200

    iMath: Implement help: Websites
    Change-Id: I983f8273fac57e1253bea00e08bda7f2535482b7

--- sfx2/source/appl/appserv.cxx
+++ sfx2/source/appl/appserv.cxx
@@ -526,6 +526,21 @@ void SfxApplication::MiscExec_Impl( SfxRequest& rReq )
             sfx2::openUriExternally(sURL, false, rReq.GetFrameWeld());
             break;
         }
+        case SID_IMATH_WIKI:
+        {
+            sfx2::openUriExternally(officecfg::Office::Common::Menus::iMathWiki_URL::get(), false, rReq.GetFrameWeld());
+            break;
+        }
+        case SID_IMATH_WEBSITE:
+        {
+            sfx2::openUriExternally(officecfg::Office::Common::Menus::iMathWebsite_URL::get(), false, rReq.GetFrameWeld());
+            break;
+        }
+        case SID_IMATH_FONTS:
+        {
+            sfx2::openUriExternally(officecfg::Office::Common::Menus::iMathFonts_URL::get(), false, rReq.GetFrameWeld());
+            break;
+        }
         case SID_DOCUMENTATION:
         {
             // Open documentation page based on locales

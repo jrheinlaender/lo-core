1eba231f7dc7b0d0f88df149b0e3afc99b64b539
Jan Rheinländer <jrheinlaender@users.sourceforge.net>
2023-04-12 19:27:42 +0200

    iMath: Cleanup
    Change-Id: I36906e6bb3a7bab2e048c9054f197e2978c07b26

--- starmath/source/mathml/mathmlimport.cxx
+++ starmath/source/mathml/mathmlimport.cxx
@@ -479,7 +479,7 @@ void SmXMLImport::endDocument()
                 } while (lineIdx >= 0);
 
                 mnImSyntaxVersion = programVersion;
-                SAL_INFO_LEVEL(2, "starmath.imath", "Result\n'" << aImText << "'");
+                SAL_INFO_LEVEL(3, "starmath.imath", "Result\n'" << aImText << "'");
             } else if (mnImSyntaxVersion < programVersion) {
                 // Document has older version than program
                 SAL_INFO_LEVEL(2, "starmath.imath", "Migrating document from version " << mnImSyntaxVersion << " to version " <<  programVersion);

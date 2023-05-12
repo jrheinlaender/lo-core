e54122a9e31ffe108e23fe29ddefbd8b2b05e4bd
Jan Rheinländer <jrheinlaender@users.sourceforge.net>
2023-05-12 18:12:09 +0200

    iMath: Some changes for Windows build
    Change-Id: Iba00070cd8e3ae45c89cee03bbdde3bdd0e6e7b6

--- starmath/source/document.cxx
+++ starmath/source/document.cxx
@@ -544,7 +544,7 @@ void SmDocShell::Compile()
     } catch (Exception &e) {
         // TODO: Show error message to user
         MSG_ERROR(0, "ERROR1: " << STR(e.Message) << endline);
-    } catch (duplication_error &e) {
+    } catch (duplication_error&) {
         // TODO: Show error message and dialog to user
         MSG_ERROR(0, "Caught duplication error" << endline);
     } catch (std::exception &e) {

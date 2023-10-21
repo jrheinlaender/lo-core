3df98054e06518cee6faa5beb4c4c2e04845552e
Jan Rheinländer <jrheinlaender@users.sourceforge.net>
2023-10-21 20:50:55 +0200

    iMath: Reset TreeView model whenever something was changed by the user
    Change-Id: Ida0edc5187682fe367da767a085052cea7eee33a

--- starmath/inc/edit.hxx
+++ starmath/inc/edit.hxx
@@ -226,7 +226,9 @@ public:
 
     SmDocShell* GetDoc();
 
-    void Update();
+    // Rebuild the TreeView model from the document
+    void ResetModel();
+
     virtual void GrabFocus();
     bool HasFocus() const;
     // Is the page of this view current in the notebook?

cadc5e2fab9f045f45a94bfeb82d12fba72f9046
Jan Rheinländer <jrheinlaender@users.sourceforge.net>
2024-03-30 20:46:11 +0100

    iMath: Code updates from extension
    Change-Id: I9bf303886b56f1268ebc5105c5dcf62802a42e08

--- imath/source/iFormulaLine.cxx
+++ imath/source/iFormulaLine.cxx
@@ -296,6 +296,9 @@ void iFormulaLine::markError(const OUString& compiledText, const int formulaStar
     int _errorEnd
         = (errorEnd > compiledText.getLength() - 1 ? compiledText.getLength() - 1 : errorEnd);
     OUString offendingText = compiledText.copy(errorStart, _errorEnd - errorStart);
+    if (errorMessage.indexOf("implicit multiplication") > 0)
+        offendingText = OUString(sal_Unicode(
+            u'\u00D7')); // Unexpected implicit multiplication // TODO implement this independently of the text of the error message
 
     _formulaParts.clear();
     _formulaParts.emplace_back(compiledText.copy(formulaStart, errorStart - formulaStart));
@@ -332,12 +335,8 @@ OUString iFormulaLine::getFormula() const
         case label_error:
             return _formulaParts[2];
         default:
-        {
-            OUString formula = OU("");
-            for (const auto& p : _formulaParts)
-                formula += p;
-            return formula;
-        }
+            // All other errors
+            return _formulaParts[0] + _formulaParts[1] + _formulaParts[2];
     }
 }
 

e371056572b63ec5cffe55559b4095b7f6fd7700
Jan Rheinländer <jrheinlaender@users.sourceforge.net>
2024-04-21 09:00:10 +0200

    iMath: Implement options dialog for iFormula lines
    Change-Id: If7c2be2b5690f7565f51fe04e07eab245177c1f3

--- starmath/source/document.cxx
+++ starmath/source/document.cxx
@@ -921,6 +921,10 @@ void SmDocShell::SetImHidden(const bool h)
 
     SAL_INFO_LEVEL(2, "starmath.imath", "Rebuilt ImText: '" << maImText << "'");
 
+const GiNaC::expression& SmDocShell::GetUnit(const OUString& unitname) const
+{
+    return mpCurrentCompiler->getUnit(STR(unitname));
+}
     if (!maImText.equals(oldImText))
     {
         if (h)
@@ -929,6 +933,11 @@ void SmDocShell::SetImHidden(const bool h)
             Compile();
     }
 
+const std::vector<std::string> SmDocShell::GetAllUnitNames() const {
+    if (mpCurrentCompiler != nullptr)
+        return mpCurrentCompiler->getUnitnames();
+
+    return {};
     mImHidden = h;
 }
 

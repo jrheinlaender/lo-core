212e701efe9f8c582971907d92a29d92da96426e
Jan Rheinländer <jrheinlaender@users.sourceforge.net>
2024-06-10 20:31:28 +0200

    iMath: Implement UNITDEF and PREFIXDEF in Gui
    Change-Id: I39063e977d07d1a8164af950c4bdfcd5c17f8642

--- starmath/UIConfig_smath.mk
+++ starmath/UIConfig_smath.mk
@@ -46,6 +46,7 @@ $(eval $(call gb_UIConfig_add_uifiles,modules/smath,\
 	starmath/uiconfig/smath/ui/symdefinedialog \
 	starmath/uiconfig/smath/ui/iformulaoptionsdialog \
 	starmath/uiconfig/smath/ui/iformulalabeldialog \
+	starmath/uiconfig/smath/ui/iformulaunitprintname \
 ))
 
 # vim: set noet sw=4 ts=4:

5b37bcc8f9f2d2cb81fefe6ea0b70233a4ff822e
Jan Rheinländer <jrheinlaender@users.sourceforge.net>
2025-03-15 16:32:19 +0100

    iMath: Update from extension
    Change-Id: I3978462720b249ede71d8f895bb62b5744342286

--- imath/source/extintegral.cxx
+++ imath/source/extintegral.cxx
@@ -484,7 +484,7 @@ void init_table() {
 /*274 is a basic function */
 /*275*/ { pow(sinax, 2), x / 2 - Functionmanager::create_hard("sin", {2 * a * x}) / (4 * a) },
 /*276*/ { pow(sinax, 3), -1 / a * cosax + 1 / (3 * a) * pow(cosax, 3) },
-/*277*/ { pow(sinax, 4), numeric(3,8) / x - 1 / (4 * a) * Functionmanager::create_hard("sin", {2 * a * x}) + 1 / (32 * a) * Functionmanager::create_hard("sin", {4 * a * x}) },
+/*277*/ { pow(sinax, 4), numeric(3,8) * x - 1 / (4 * a) * Functionmanager::create_hard("sin", {2 * a * x}) + 1 / (32 * a) * Functionmanager::create_hard("sin", {4 * a * x}) },
 /*278 is a recursive partial integral*/
 /*279*/ { x * sinax, sinax / pow(a, 2) - x * cosax / a },
 /*280*/ { pow(x, 2) * sinax, 2 * x / pow(a, 2) * sinax - (pow(x, 2) / a - 2 / pow(a, 3)) * cosax },
@@ -529,7 +529,7 @@ void init_table() {
 /*313 is a basic function */
 /*314*/ { pow(cosax, 2), x / 2 + Functionmanager::create_hard("sin", {2 * a * x}) / (4 * a) },
 /*315*/ { pow(cosax, 3), 1 / a * sinax - pow(sinax, 3) / (3 * a) },
-/*316*/ { pow(cosax, 4), numeric(3, 8) * x + 1 / (4 * a) * Functionmanager::create_hard("sin", {2 * a * x}) + pow(Functionmanager::create_hard("sin", { 4 * a * x}), 3) / (32 * a) },
+/*316*/ { pow(cosax, 4), numeric(3, 8) * x + 1 / (4 * a) * Functionmanager::create_hard("sin", {2 * a * x}) + Functionmanager::create_hard("sin", { 4 * a * x}) / (32 * a) },
 /*317 is a recursive partial integral */
 /*318*/ { x * cosax, cosax / pow(a, 2) + x * sinax / a },
 /*319*/ { pow(x, 2) * cosax, 2 * x / pow(a, 2) * cosax + (pow(x, 2) / a - 2 / pow(a, 3)) * sinax },
@@ -562,7 +562,7 @@ void init_table() {
 /*341*/ { pow(1 - cosax, -2), -1 / (2 * a) * 1 / Functionmanager::create_hard("tan", {a * x / 2}) - 1 / (6 * a) * pow(Functionmanager::create_hard("tan", {a * x / 2}), -3) },
 /*342*/ { cosax * pow(1 + cosax, -2), 1 / (2 * a) * Functionmanager::create_hard("tan", {a * x / 2}) - 1 / (6 * a) * pow(Functionmanager::create_hard("tan", {a * x / 2}), 3) },
 /*343*/ { cosax * pow(1 - cosax, -2), 1 / (2 * a) * 1 / Functionmanager::create_hard("tan", {a * x / 2}) - 1 / (6 * a) * pow(Functionmanager::create_hard("tan", {a * x / 2}), -3) },
-/*344*/ { pow(1 + pow(cosax, 2), -1), 1 / (2 * GiNaC::sqrt(numeric(2)) * a)* Functionmanager::create_hard("arcsin", {(1 - 3 * pow(cosax, 2)) / (1 + pow(cosax, 2))}) },
+/*344*/ { pow(1 + pow(cosax, 2), -1), 1 / (2 * GiNaC::sqrt(ex(2)) * a)* Functionmanager::create_hard("arcsin", {(1 - 3 * pow(cosax, 2)) / (1 + pow(cosax, 2))}) },
 /*345*/ { pow(1 - pow(cosax, 2), -1), -1 / (tanax * a) },
 /*345*/ { pow(pow(cosax, 2) - 1, -1), +1 / (tanax * a) },
 /*354*/ { ex(sinax) * ex(cosax), pow(sinax, 2) / (2 * a) },

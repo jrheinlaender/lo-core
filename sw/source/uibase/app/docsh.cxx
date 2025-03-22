f92a7471ed4a80f92de82bf652036c67adbce8eb
Jan Rheinländer <jrheinlaender@users.sourceforge.net>
2025-03-22 19:33:05 +0100

    iMath: Fix rebase errors
    Change-Id: Iefe0db9c6930b661ee6c4505c7140b13eed22332

--- sw/source/uibase/app/docsh.cxx
+++ sw/source/uibase/app/docsh.cxx
@@ -1516,10 +1516,8 @@ void SwDocShell::RecalculateDependentIFormulas(const OUString& formulaName)
             updateFormatting(xFormulaComp); // Update formula properties autotextmode, margin
         }
 
-
         ++it;
     }
-
 
     return;
 }

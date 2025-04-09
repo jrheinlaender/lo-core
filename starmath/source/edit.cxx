2701431c99e2791f05be91d14ebb77273803434d
Jan Rheinländer <jrheinlaender@users.sourceforge.net>
2025-04-09 20:26:14 +0200

    iMath: Remove call to non-existing method
    Change-Id: If43ef304fc6da6d01c5492f14b8445874bc59a12

--- starmath/source/edit.cxx
+++ starmath/source/edit.cxx
@@ -2102,10 +2102,6 @@ void AbstractEditTextWindow::GetFocus()
     EditEngine *pEditEngine = GetEditEngine();
     if (pEditEngine)
         pEditEngine->SetStatusEventHdl(LINK(this, AbstractEditTextWindow, EditStatusHdl));
-
-    //Let SmViewShell know we got focus
-    if (mrEditWindow.GetView() && SmViewShell::IsInlineEditEnabled())
-        mrEditWindow.GetView()->SetInsertIntoEditWindow(true);
 }
 
 void AbstractEditTextWindow::LoseFocus()

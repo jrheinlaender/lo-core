8f6599a967faeee8a118b194d9202923583907ff
Jan Rheinländer <jrheinlaender@users.sourceforge.net>
2025-06-02 19:53:08 +0200

    iMath Code updates from extension
    Change-Id: I1f8a467c9d04e787fae84ed246398460e1c34aec

--- include/imath/iFormulaLine.hxx
+++ include/imath/iFormulaLine.hxx
@@ -127,6 +127,7 @@ public:
     virtual OUString printFormula() const;
     OUString getFormula() const;
     void setFormula(const OUString& f);
+    void setFormula(std::vector<OUString> formulaParts);
 
     /// Return the message if the line has an error
     OUString getErrorMessage() const;

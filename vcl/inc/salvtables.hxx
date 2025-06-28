52a2646367f28df77affc8db7653a34bc33d910a
Jan Rheinländer <jrheinlaender@users.sourceforge.net>
2025-06-28 16:35:19 +0200

    iMath: Fix issues with IterString
    Change-Id: Ifb26d15c03bb002d57167d9b2f2b1aaea0a9dd2b

--- vcl/inc/salvtables.hxx
+++ vcl/inc/salvtables.hxx
@@ -1480,6 +1480,8 @@ struct SalInstanceTreeIter final : public weld::TreeIter
 
 class SalInstanceTreeView : public SalInstanceWidget, public virtual weld::TreeView
 {
+public:
+    typedef std::pair<SvTreeListEntry*, OUString> IterString;
 protected:
     // owner for UserData
     std::vector<std::unique_ptr<OUString>> m_aUserData;
@@ -1505,7 +1507,6 @@ protected:
     DECL_LINK(StartDragHdl, SvTreeListBox*, bool);
     DECL_STATIC_LINK(SalInstanceTreeView, FinishDragHdl, SvTreeListBox*, void);
     DECL_LINK(EditingEntryHdl, SvTreeListEntry*, bool);
-    typedef std::pair<SvTreeListEntry*, OUString> IterString;
     DECL_LINK(EditedEntryHdl, IterString, bool);
     DECL_LINK(EditingCanceledHdl, IterString, void);
     DECL_LINK(VisibleRangeChangedHdl, SvTreeListBox*, void);
